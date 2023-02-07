1185c688f0a7ceaaf9ab5d5dbe350829034c28ed
jamaldemir <jamal@cybertekschool.com>
2023-02-07 14:52:11 -0500

    Excel related files added

--- src/test/java/com/cydeo/day11/P06_MethodSourceTest.java
+++ src/test/java/com/cydeo/day11/P06_MethodSourceTest.java
@@ -1,10 +1,12 @@
 package com.cydeo.day11;
 
+import com.cydeo.utilities.ExcelUtil;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class P06_MethodSourceTest {
 
@@ -13,6 +15,23 @@ public class P06_MethodSourceTest {
     public void test1(String name){
         System.out.println("name = " + name);
 
+    }
+
+    @ParameterizedTest
+    @MethodSource("getExcelData")
+    public void test2(Map<String,String> userInfo){
+        System.out.println("userInfo = " + userInfo);
+        System.out.println("userInfo.get(\"Email\") = " + userInfo.get("Email"));
+        System.out.println("userInfo.get(\"Password\") = " + userInfo.get("Password"));
+
+
+    }
+
+    public static List<Map<String,String>> getExcelData(){
+
+        ExcelUtil excelUtil = new ExcelUtil("src/test/resources/Library.xlsx","Library1");
+
+        return excelUtil.getDataList();
     }
 
 
